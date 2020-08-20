45d46abc54928449a214cd4a0d00bf9e275e7f68
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-08-20 20:06:33 +0100

    #1building home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header'
+import Header from '../components/Header';
+import protestImg from '../images/sa.jpg'
 
 
 
@@ -9,12 +10,26 @@ const Home = () => {
     return (
         <>
             <Header></Header>
+            <main className="h-main">
+                <div className="h-main-page">
+                    <section className="h-div-tp intro">
+                        <div className="intro-txt-div">
+                            <h2>
+                            See a societal situation in need of change?
+                            </h2>
+                        </div>
+                        <div className="intro-img-div">
+                            <img src={protestImg} alt="" />
+                        </div>
+                    </section>
+                    <div className="clear"></div>
+                    <section className="h-div-md"></section>
+                    <div className="h-div-bt"></div>
+                </div>
+            </main>
 
-            <div>
-                I am home
-            <span><Link to="/auth">  SignUp</Link></span>
 
-            </div>
+
         </>
     );
 }
