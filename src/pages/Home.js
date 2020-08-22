17a5c574c76da26d7177ca352e4b3c88649fff83
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-08-22 21:26:54 +0100

    Done with board component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import protestImg from '../images/sa.jpg'
-import { FaSistrix } from 'react-icons/fa';
+import { FaSistrix, FaUser, FaTabletAlt, FaPaperPlane, FaMobileAlt, FaDesktop, FaAt } from 'react-icons/fa';
 
 
 
@@ -62,14 +62,44 @@ const Home = () => {
                                     <div className="name">
                                         <p >iReporter</p>
                                     </div>
-                                    <div>
-                                        <FaSistrix />
-                                        <p></p>
+                                    <div className="sistrix-div">
+                                        <p><FaSistrix className="sistrix" /></p>
+                                        <p>Profile</p>
+                                        <p>H</p>
                                     </div>
                                 </div>
                                 <div className="board-main">
+                                    <div className="txt-div">
+                                        <h4>How it works.</h4>
+                                        <div className="lines"></div>
+                                        <div className="lines"></div>
+                                        <div className="lines"></div>
+                                        <div className="lines"></div>
+                                    </div>
+                                    <div className="card-div">
+                                        <article >
+                                            <FaTabletAlt className="icon"></FaTabletAlt>
+                                            <p className="txt">Sign up with your details.</p>
+                                        </article>
+
+                                        <article className="card">
+                                            <FaAt className="icon"></FaAt>
+                                            <p className="txt"> Verify account with code sent to your email.</p>
+                                        </article>
+
+                                        <article className="card">
+                                            <FaPaperPlane className="icon"></FaPaperPlane>
+                                            <p className="txt">Take-off!</p>
+                                        </article>
+
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <div className="mission-statement">
+                            <h2>Our mission</h2>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores nihil neque ad sequi culpa dicta non 
+                            voluptatem quas nam! Culpa autem sequi repellendus exercitationem atque dolorem nemo totam libero consequuntur.</p>
                         </div>
                     </section>
                     <div className="h-div-bt"></div>
