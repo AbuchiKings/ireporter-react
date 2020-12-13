cd75aa57eaffa74a136276b7b3602028e412a909
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-12-13 13:55:03 +0100

    Rmove immutable state invariant

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from './reducers';
-//import { reduxImmutableStateInvariant } from 'redux-immutable-state-invariant';
 
 
 export default function configureState(initialState) {
