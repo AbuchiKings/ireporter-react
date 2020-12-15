7647fe7137aca016ab803a525fbe54ebe681a4e9
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-12-15 23:12:53 +0100

    Added thunk and request function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 
 export default function configureState(initialState) {
@@ -7,6 +8,6 @@ export default function configureState(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware())
+        composeEnhancers(applyMiddleware(thunk))
     );
 }
---
+++ src/util/request.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+/**
+ * Helper function that configures axios requests
+ * @param {object} config The configuration data.
+ * route - The route to make request to. e.g profiles, search?q=term&size=5
+ * method - The HTTP method to use. e.g post, get, patch,
+ * payload - The request body payload
+ * token - The authorization token to use if any
+ * @returns {Promise} The axios promise
+ */
+
+const baseUrl = process.env.REACT_APP_API_URL;
+const request = async ({ route, verb, payload = undefined }) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  const method = verb || 'get';
+  const headers = { 'Authorization': `${Bearer} ${token}` };
+  if (!token) {
+    delete headers.Authorization;
+  }
+  return axios({
+    data: payload,
+    url: `${baseUrl}${route}`,
+    method,
+    headers,
+  });
+};
+
+export default request;
