b53cbadc3037f8921f4fdd4f3989cc65524096ad
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-12-28 23:34:36 +0100

    Added label detail

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,78 @@
-import React from 'react'
+import React from 'react';
+import protestImg from '../images/sa.jpg'
+
 
 export default function Profile() {
     return (
-        <main className="dashboard-main">
+        <main className="profile-main">
             <div className="div-middle">
                 <div className="aside-nav">
                     <h2>Aside</h2>
                 </div>
                 <div className="main-content" >
-                    <h2>Main</h2>
+                    <div className="profile-header-div">
+                        <h1>Personal Information</h1>
+                        <p className="extra">Update your profile information.</p>
+                    </div>
+
+                    <div className="profile-details-div">
+                        <div>
+                            <div className="details-label">
+                                <h6>Profile picture</h6>
+                                <button className="extra label-button"> Choose a new profile picture</button>
+                            </div>
+
+                            <div className="details dp-div">
+                                <img src={protestImg} alt="" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="details-label">
+                                <label htmlFor="firstname">Firstname</label>
+                                <button className="extra label-button">Change first name</button>
+
+                            </div>
+
+                            <div className="details">
+                                <input type="text" name="firstname" id="" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="details-label">
+                                <label htmlFor="lastname">Lastname</label>
+                                <button className="extra label-button">Change last name</button>
+
+                            </div>
+
+                            <div className="details">
+                                <input type="text" name="lastname" id="" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="details-label">
+                                <label htmlFor="email">Email</label>
+                                <button className="extra label-button">Change email address</button>
+
+                            </div>
+
+                            <div className="details">
+                                <input type="email"
+                                    aria-label="email" name="email" id="email"
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="details-label">
+                                <label htmlFor="phone-number">Phone number</label>
+                                <button className="extra label-button"> Change phone number</button>
+                            </div>
+
+                            <div className="details">
+                                <input type="text" aria-label="phone-number" name="phone_number" id="phone-number" />
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </main>
