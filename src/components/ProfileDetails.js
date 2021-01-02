f6e06e20b0ed87dfdd1b2420e5ae4859c2a29b7f
Abuchi Kingsley <abuchikingsley76@gmail.com>
2021-01-02 11:20:48 +0100

    Moved profile details to separate component

---
+++ src/components/ProfileDetails.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import boyImg from '../images/boy-with-book.jpg'
+
+export default function ProfileDetails() {
+    return (
+        <form action="" className="form-content" noValidate>
+            <div className="profile-details-div">
+                <div>
+                    <div className="details-label">
+                        <h6>Profile picture</h6>
+                        <button className="extra label-button"> Choose a new profile picture</button>
+                    </div>
+
+                    <div className="details dp-div">
+                        <img src={boyImg} alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div className="details-label">
+                        <label htmlFor="firstname">Firstname</label>
+                        <button className="extra label-button">Change first name</button>
+
+                    </div>
+
+                    <div className="details">
+                        <input type="text" name="firstname" value="Abuchi" className="profile-input" />
+                    </div>
+                </div>
+                <div>
+                    <div className="details-label">
+                        <label htmlFor="lastname">Lastname</label>
+                        <button className="extra label-button">Change last name</button>
+
+                    </div>
+
+                    <div className="details">
+                        <input type="text" name="lastname" id="lastname" value="Ndinigwe" />
+                    </div>
+                </div>
+                <div>
+                    <div className="details-label">
+                        <label htmlFor="email">Email</label>
+                        <button className="extra label-button">Change email address</button>
+
+                    </div>
+
+                    <div className="details">
+                        <input type="email"
+                            aria-label="email" name="email" id="email" value="abuchikingsley76@gmail.com"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div className="details-label">
+                        <label htmlFor="phone-number">Phone number</label>
+                        <button className="extra label-button"> Change phone number</button>
+                    </div>
+
+                    <div className="details">
+                        <input type="text" aria-label="phone-number" name="phone_number" id="phone-number" value="+2348062158380" />
+                    </div>
+                </div>
+                <div className="btn-top-div">
+                    <div className="details btn-div">
+                        <button type="submit" className="btn btn-primary profile-edit-btn">Edit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import boyImg from '../images/boy-with-book.jpg'
+import ProfileDetails from '../components/ProfileDetails';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 
-export default function Profile() {
+
+export default function Profile({ location, user, history }) {
     return (
         <main className="profile-main">
             <div className="div-middle">
@@ -10,77 +12,27 @@ export default function Profile() {
                     <h2>Aside</h2>
                 </div>
                 <div className="main-content" >
+
                     <div className="profile-header-div">
                         <h1>Personal Information</h1>
                         <p className="extra">Update your profile information.</p>
                     </div>
-
-                    <div className="profile-details-div">
-                        <div>
-                            <div className="details-label">
-                                <h6>Profile picture</h6>
-                                <button className="extra label-button"> Choose a new profile picture</button>
-                            </div>
-
-                            <div className="details dp-div">
-                                <img src={boyImg} alt="" />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="details-label">
-                                <label htmlFor="firstname">Firstname</label>
-                                <button className="extra label-button">Change first name</button>
-
-                            </div>
-
-                            <div className="details">
-                                <input type="text" name="firstname" id="" value="Abuchi" className="profile-input"/>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="details-label">
-                                <label htmlFor="lastname">Lastname</label>
-                                <button className="extra label-button">Change last name</button>
-
-                            </div>
-
-                            <div className="details">
-                                <input type="text" name="lastname" id="" value="Ndinigwe"/>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="details-label">
-                                <label htmlFor="email">Email</label>
-                                <button className="extra label-button">Change email address</button>
-
-                            </div>
-
-                            <div className="details">
-                                <input type="email"
-                                    aria-label="email" name="email" id="email" value="abuchikingsley76@gmail.com"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <div className="details-label">
-                                <label htmlFor="phone-number">Phone number</label>
-                                <button className="extra label-button"> Change phone number</button>
-                            </div>
-
-                            <div className="details">
-                                <input type="text" aria-label="phone-number" name="phone_number" id="phone-number" value="+2348062158380" />
-                            </div>
-                        </div>
-                        <div>
-
-                            <div className="details">
-                            <button type="submit" className="btn btn-primary">Edit</button>
-                            </div>
-                        </div>
-                    </div>
-
+                    <Switch>
+                        <Route exact path="/profile/details" render={() => (
+                            <ProfileDetails />
+                        )} />
+                        <Redirect from="/profile" to="/profile/details" />
+                    </Switch>
                 </div>
+
             </div>
         </main>
     )
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    };
 }
